6aa430006ce43a3a84ae16ec69aae1fc51662c08
zahirbegulic@gmail.com <zahirbegulic@gmail.com>
2021-03-10 18:55:19 +0100

    Declared three variables (x, y and z) in index.js file (z variable edited)

--- ex06/src/index.js
+++ ex06/src/index.js
@@ -1,11 +1,11 @@
 function main(){
 	var x = 7;
 	var y = 12;
-	var z = "My name is ";
+	var z = "My name is";
 
 	x = x + 2;
 	y = y + 4;
-	z = z + "John!";
+	z = z + " John!";
 	
 	return [x, y, z];
 }
